7c4b68971280b622063e978c1b8cd303e1a2c4a0
Jocareher <jocareher95@gmail.com>
2024-01-29 13:48:28 +0100

    Update FaceTrainer with RotatedCOCOEvaluator

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from detectron2.engine import DefaultTrainer
-from detectron2.evaluation import COCOEvaluator
+from detectron2.evaluation import RotatedCOCOEvaluator, DatasetEvaluators
 from detectron2.data import build_detection_train_loader
 from detectron2.config import CfgNode
 
@@ -17,18 +17,26 @@ class FaceTrainer(DefaultTrainer):
         output_folder: Optional[str] = None,
     ):
         """
-        Creates an evaluator for the specified dataset.
+        Instantiate an evaluator for rotated bounding box detection on a given dataset.
+        This method overrides the DefaultTrainer's build_evaluator method to provide
+        a COCO-style evaluator that supports the evaluation of rotated bounding boxes.
 
         Args:
-            cfg (CfgNode): Configuration for the model and training.
-            dataset_name (str): Name of the dataset for evaluation.
-            output_folder (Optional[str]): Output folder to store evaluation results.
+            cfg (CfgNode): Configuration Node object containing the model and training configurations.
+            dataset_name (str): The name of the dataset to be evaluated.
+            output_folder (Optional[str], optional): The directory where the evaluation results will be stored.
+                If not provided, the results won't be saved to disk.
 
         Returns:
-            COCOEvaluator: A COCO evaluator for the dataset.
+            DatasetEvaluators: An object containing a list of evaluators, with a RotatedCOCOEvaluator
+            for evaluating datasets with rotated bounding boxes in COCO format. This object can be
+            used to evaluate the dataset during training or inference.
         """
-        # Return a COCO evaluator for the specified dataset
-        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+        # Instantiate the RotatedCOCOEvaluator
+        evaluator = RotatedCOCOEvaluator(dataset_name, cfg, True, output_folder)
+        
+        # Return a DatasetEvaluators object containing the list of evaluators
+        return DatasetEvaluators([evaluator])
 
     @classmethod
     def build_train_loader(cls, cfg: CfgNode):

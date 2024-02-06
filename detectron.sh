8fe6c144952f1cb5368784387af0de1de7de57b6
Jocareher <jocareher95@gmail.com>
2024-02-06 18:06:10 +0100

    Update last input args

--- detectron.sh
+++ detectron.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J detectron
-#SBATCH -p short
+#SBATCH -p high
 #SBATCH -N 1
 #### SBATCH --nodelist=node026
 #SBATCH --chdir=/home/jreyes/face_detectron2
 #SBATCH --gres=gpu:tesla:1
-#SBATCH -o /home/jreyes/face_detectron2/detectron_fe%J.%u.out # STDOUT
-#SBATCH -e /home/jreyes/face_detectron2/detectron_fe%J.%u.err # STDERR
+#SBATCH -o /home/jreyes/Baby_face_detection/detectron_pw%J.%u.out # STDOUT
+#SBATCH -e /home/jreyes/Baby_face_detection/detectron_pw%J.%u.err # STDERR
 
 # Load CUDA module
 module load CUDA/12.2.2
@@ -29,4 +29,4 @@ pip install --user -r requirements.txt
 
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
-python3 ./scripts/main.py --ims_per_batch 2 --batch_size_per_image 128 --num_workers 2
+python3 ./scripts/main.py --ims_per_batch 32 --batch_size_per_image 128 --num_workers 2 --unfreeze_backbone True --freeze_at_block 3 --output_dir "./output_pw"

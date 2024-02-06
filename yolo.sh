303c0119bb8519d5e9a442fe4419666f4a1134d6
Jocareher <jocareher95@gmail.com>
2024-02-06 18:26:59 +0100

    Add different scenarios to yolo.sh

--- yolo.sh
+++ yolo.sh
@@ -26,4 +26,13 @@ source activate yolo
 
 pip install ultralytics
 
+# # Build a new model from YAML and start training from scratch
+# yolo detect train data=coco128.yaml model=yolov8n.yaml epochs=100 imgsz=640
+
+# # Start training from a pretrained *.pt model
+# yolo detect train data=coco128.yaml model=yolov8n.pt epochs=100 imgsz=640
+
+# # Build a new model from YAML, transfer pretrained weights to it and start training
+# yolo detect train data=coco128.yaml model=yolov8n.yaml pretrained=yolov8n.pt epochs=100 imgsz=640
+
 yolo task=obb mode=train model=yolov8s-obb.pt data=./configs/yolo.yaml epochs=100 imgsz=640 batch=16
